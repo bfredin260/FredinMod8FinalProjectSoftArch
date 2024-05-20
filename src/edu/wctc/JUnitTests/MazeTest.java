f2f3d3975959fc1098789bbcc3cf391c51bbe75d
Braedon Fredin <b260fredin@gmail.com>
2024-05-19 21:24:40 -0500

    [0.5.0] Added MazeTest class using JUnit.

---
+++ src/edu/wctc/JUnitTests/MazeTest.java
@@ -0,0 +1,28 @@
+package edu.wctc.JUnitTests;
+
+import edu.wctc.Maze;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MazeTest {
+    Maze testMaze;
+
+    @BeforeEach
+    void setUp() {
+        testMaze = Maze.getInstance();
+    }
+
+    @Test
+    void canMoveInvalidTest() {
+        // currentRoom has no connection on its south side
+        assertFalse(testMaze.canMove('s'));
+    }
+
+    @Test
+    void canMoveValidTest() {
+        // currentRoom DOES have a connection on its north side
+        assertFalse(testMaze.canMove('n'));
+    }
+}
